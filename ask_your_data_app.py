786b1a29c00c8fdc77f7aa217d921ede13276db4
nik-db <databite07@gmail.com>
2025-06-18 15:16:18 +0530

    Update ask_your_data_app.py

--- ask_your_data_app.py
+++ ask_your_data_app.py
@@ -11,8 +11,8 @@ from openai import OpenAI
 load_dotenv()
 
 # Use OpenRouter API
-openai.api_key = os.getenv("OPENROUTER_API_KEY")
-openai.api_base = "https://openrouter.ai/api/v1"
+# openai.api_key = os.getenv("OPENROUTER_API_KEY")
+# openai.api_base = "https://openrouter.ai/api/v1"
 client = OpenAI(api_key=st.secrets["OPENROUTER_API_KEY"], base_url="https://openrouter.ai/api/v1")
 
 # Page setup
